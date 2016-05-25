56ac931fdcf4b0d9083c0e0e836ee19faf773e0a
Atul <kkmonlee@gmail.com>
2016-05-25 02:55:11 +0100

    Working implementation of a Stack Signed-off-by:Atul <kkmonlee@gmail.com>

--- src/ufp52_5045/UFP52_5045.java
+++ src/ufp52_5045/UFP52_5045.java
@@ -15,7 +15,13 @@ public class UFP52_5045 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Stack stack = new Stack();
+        
+        stack.push(80);
+        stack.push(28);
+        stack.push(231);
+        stack.pop();
         
         
     }
