40e06222c9045f5db9b85899c3bba139ac2ef434
Atul <kkmonlee@gmail.com>
2016-05-26 02:58:31 +0100

    Few changes Signed-off-by:Atul <kkmonlee@gmail.com>

---
+++ src/ufp52_5045/Stack.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufp52_5045;
+
+/**
+ *
+ * @author master
+ */
+public class Stack {
+    
+    
+    int pointer;
+    final int capacity = 5;
+    int arr[] = new int[capacity];
+    
+    Stack() {
+        
+        pointer = -1;
+    }
+    
+    
+    
+    void push(int element) {
+        if (pointer < capacity -1) {
+            
+            pointer++;
+            arr[pointer] = element;
+            
+            System.out.println("Element " + element + " has been pushed onto the Stack. \n");
+            print();
+        } else {
+            System.out.println("Stack overflow! \n");
+        }
+    }
+    
+    void pop() {
+        
+        if (pointer >= 0) {
+            pointer--;
+            System.out.println("The latest element has been removed/popped. \n");
+            print();
+        } else {
+            System.out.println("Stack underflow! \n");
+        }
+    }
+    
+    void print() {
+        
+        if (pointer >= 0) {
+            System.out.println("Elements in the stack are: \n");
+            
+            for (int i = 0; i <= pointer; i++) {
+                System.out.println(arr[i]);
+            }
+            
+            
+        }
+        
+    }
+    
+}
--- src/ufp52_5045/UFP52_5045.java
+++ src/ufp52_5045/UFP52_5045.java
@@ -15,21 +15,29 @@ public class UFP52_5045 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /**
-        Stack stack = new Stack();
+        
+        /**Stack stack = new Stack();
         
         stack.push(80);
         stack.push(28);
         stack.push(231);
+        stack.print();
         stack.pop();
         **/
         
-        CircularQueue queue = new CircularQueue(5);
-        queue.insert(78);
-        queue.insert(123);
-        queue.print();
-        queue.delete();
-        queue.print();
+        
+//        CircularQueue queue = new CircularQueue();
+//        queue.print();
+//        queue.insert(78);
+//        queue.print();
+//        queue.delete();
+//        queue.print();
+//        queue.insert(99);
+//        queue.print();
+       
+        SierpinskiCarpet carpet = new SierpinskiCarpet();
+        carpet.run();
+        
         
     }
     
