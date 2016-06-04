8f9d0ef24f45e40e0bdadabb65d4d83b2a560f70
kkmonlee <kkmonlee@gmail.com>
2016-06-04 03:18:20 +0100

    Implementation of Linked List using arrays
    Created the class's own main method so it's easier.
    I think I'll start making main methods for each class so I don't have to comment out code in the project's main method.

---
+++ src/ufp52_5045/LinkedList.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufp52_5045;
+
+/**
+ *
+ * @author Atul Anand Sinha
+ */
+public class LinkedList {
+    final int capacity = 10;
+    String item[] = new String[capacity];
+    int itemSize = 0;
+    int currentLocation;
+    
+    void addItems(String element) {
+        if (element != null) {
+            item[itemSize] = element;
+            itemSize++;
+        }
+    }
+    
+    void deleteItems(String element) {
+        int flag = 0;
+        if (itemSize > 0) {
+            for (int i = 0; i < itemSize; i++) {
+                if (item[i].equals(element)) {
+                    flag = 1;
+                    currentLocation = i;
+                    break;
+                }
+            }
+            
+            if (flag == 0) {
+                System.out.println("No such element to delete such as '" + element + "'");
+            } else if (flag == 1) {
+                for (int i = currentLocation + 1; i < itemSize; i++) {
+                    item[currentLocation] = item[i];
+                    currentLocation++;
+                }
+                itemSize--;
+                System.out.println("'" + element + "' element is deleted.");
+            }
+        }
+    }
+    
+    void viewItems() {
+        if (itemSize > 0) {
+            System.out.println("The elements in the Linked List are: ");
+            for (int i = 0; i < itemSize; i++) {
+                System.out.print(item[i] + ", ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main (String a[]) {
+        LinkedList list = new LinkedList();
+        
+        list.addItems("a");
+        list.addItems("b");
+        list.addItems("c");
+        list.viewItems();
+        
+        list.deleteItems("b");
+        list.viewItems();
+    }
+}
