90e566b009b88096e3d96cdf33dab05ee92cf884
kkmonlee <kkmonlee@gmail.com>
2016-06-16 20:46:12 +0100

    Shell Sort in progress

---
+++ src/ufp52_5045/ShellSort.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufp52_5045;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Atul Anand Sinha
+ */
+public class ShellSort {
+    
+    public static void main(String[] args) {
+        int[] array = {5, 3, 0, 2, 4, 1, 0, 5, 2, 3, 1, 4};
+        System.out.println("Before: " + Arrays.toString(array));
+        //shellSort(array);
+        System.out.println("After: " + Arrays.toString(array));
+    }
+}
